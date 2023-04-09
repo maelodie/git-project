efc71c86de55f6f86ecbb7aa5f588b9b73faa9e3
Ramahatafandry_Maeva <henintsoa.ramahatafandry@etu.sorbonne-universite.fr>
2023-04-09 22:10:08 +0200

    Exo4 fini

--- exo4/worktree.c
+++ exo4/worktree.c
@@ -11,9 +11,12 @@ WorkFile* createWorkFile(char* name) {
     return new;
 }
 
+void printWorkFile(WorkFile* wf) {
+    printf("nom: %s\nhash: %s\nmode: %d\n", wf->name, wf->hash, wf->mode);
+}
 char* wtfs(WorkFile* wf) {
     char* res = (char*)malloc(sizeof(char)*1000);
-    sprintf(res,"%s\t%s\t%d", wf->name, wf->mode, wf->name);
+    sprintf(res,"%s\t%s\t%d", wf->name, wf->hash, wf->mode);
     return res;
 }
 
@@ -21,8 +24,8 @@ WorkFile* stwf(char* ch) {
     char* name = (char*)malloc(sizeof(char)*100);
     char* hash = (char*)malloc(sizeof(char)*100);
     int mode;
-    sscanf(ch,"%s\t%s\t%d", name, hash, mode);
-    WorkFile res = createWorkFile(name);
+    sscanf(ch,"%s\t%s\t%d", name, hash, &mode);
+    WorkFile* res = createWorkFile(name);
     res->hash = hash;
     res->mode = mode;
     return res;
@@ -37,17 +40,99 @@ WorkTree* initWorkTree() {
 }
 
 int inWorkTree(WorkTree* wt, char* name) {
-    int pos = 0;
     for(int i = 0; i < (wt->size); i++) {
-        if((wt->tab[i]).name == name) return i;
+        if((wt->tab[i]).name == name) {
+            return i;
+        }
     }
     return -1;
 }
 
 int appendWorkTree(WorkTree* wt, char* name, char* hash, int mode) {
-    if(inWorkTree(name) > -1) {
+    if(inWorkTree(wt, name) > -1) {
         printf("Existe déjà\n");
         return -1;
     }
-    if()
+    if(wt->size > wt->n) {
+        wt->tab[wt->n].name = strdup(name);
+        wt->tab[wt->n].mode = mode;
+        if(hash != NULL) {
+            wt->tab[wt->n].hash = strdup(hash);
+        }
+        else {
+            wt->tab[wt->n].hash = NULL;
+        }
+        (wt->n)++;
+        return 0;
+    }
+    else {
+        printf("Le tableau est rempli\n");
+        return -1;
+    }
+}
+
+void printWorkTree(WorkTree* wt) {
+    for(int i = 0; i < wt->n; i++) {
+        printWorkFile(&(wt->tab[i]));
+    }
+}
+
+char* wtts(WorkTree* wt) {
+    char* res = (char*)malloc(sizeof(char)*1000);
+    char* tabVal;
+    char* sep = "\n";
+    for(int i = 0; i < wt->n; i++) {
+        tabVal = wtfs(&(wt->tab[i]));
+        strcat(res, tabVal);
+        strcat(res, sep);
+    }
+    return res;
+}
+
+
+WorkTree* stwt(char* ch) {
+    WorkTree* res = initWorkTree();
+    char* fin = ch;
+    char* deb = ch;
+    char* workfile;
+    WorkFile* wf;
+    while(*fin != '\0') {
+        if(*fin == '\n') {
+            workfile = strndup(deb, fin - deb);
+            wf = stwf(workfile);
+            appendWorkTree(res, wf->name, wf->hash, wf->mode);
+            deb = fin + 1;
+        }
+        fin++;
+    }
+    return res;
+}
+
+int wttf(WorkTree* wt, char* file) {
+    FILE* f = fopen(file, "w");
+    if(f) {
+        fprintf(f, "%s", wtts(wt));
+        fclose(f);
+        return 0;
+    }
+    return -1;
+}
+
+WorkTree* ftwt(char* file) {
+    FILE* f = fopen(file, "r");
+    char* buffer = (char*)malloc(sizeof(char)*1000);
+    char* res = (char*)malloc(sizeof(char)*1000*W_MAX);
+    while(fgets(buffer, W_MAX, f)) {
+        strcat(res, buffer);
+    }
+    return stwt(res);
+}
+
+void freeWorkFile(WorkFile* wf) {
+    free(wf);
+}
+
+void freeWorkTree(WorkTree* wt) {
+    free(wt->tab);
+    free(wt);
 }
---
+++ exo4/worktree_main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "worktree.h"
+
+void sep() {
+    printf("_______________________________________________\n");
+}
+
+int main(void) {
+    //Création de workfiles
+    WorkFile* wf1 = createWorkFile("mondoc1");
+    WorkFile* wf2 = createWorkFile("mondoc2");
+    WorkFile* wf3 = createWorkFile("mondoc3");
+    WorkFile* wf4 = createWorkFile("mondoc4");
+
+    //Affichage
+    printWorkFile(wf1);
+    printWorkFile(wf2);
+    printWorkFile(wf3);
+    printWorkFile(wf4);
+    sep();
+    //Conversion d'un workfile en une chaine
+    printf("%s\n",wtfs(wf1));
+    sep();
+    //Conversion d'une chaine en un Workfile
+    WorkFile* wf5 = stwf("mondoc sk34zf134141ffdaaf 2");
+    printWorkFile(wf5);
+    sep();
+
+    //Initialisation d'un WorkTree
+    WorkTree* wt = initWorkTree();
+    appendWorkTree(wt, "dossier1", "dfqdlkqjf1431431", 2);
+    appendWorkTree(wt, "dossier2", "qdfqdf42fq341341", 1);
+    appendWorkTree(wt, "dossier3", "aiaeraerfkdjld13", 2);
+    appendWorkTree(wt, "dossier4", "fdsflkjfoinq1341", 4);
+
+    //Affichage d'un WorkTree
+    printWorkTree(wt);
+    sep();
+
+    //Création d'un worktree à partir d'une chaine 
+    char* ch = "doc1 dflkqjdflkqj141dsfq 1\ndoc2 fqdjfq134fdfqfdqfdf 3\ndoc3 dqjkfqjdfqkjdf13414 2";
+    WorkTree* wt2 = stwt(ch);
+    printWorkTree(wt2);
+    sep();
+    
+    //Input des données dans un file
+    wttf(wt, "file.txt");
+
+    //Création d'un WorkTree à partir d'un file
+    WorkTree* wt3 = ftwt("file.txt");
+    printWorkTree(wt3);
+
+    //Désallocation des données
+    freeWorkFile(wf1);
+    freeWorkFile(wf2);
+    freeWorkFile(wf3);
+    freeWorkFile(wf4);
+    freeWorkFile(wf5);
+    freeWorkTree(wt);
+    freeWorkTree(wt2);
+    freeWorkTree(wt3);
+
+    return 0;
+}
